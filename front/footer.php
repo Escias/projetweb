a0d52368c3d89d22b7e52256f8157b8aa6b84bd9
Jimmy <ljimmy98@hotmail.fr>
2020-06-09 11:59:35 +0200

    change to php file

---
+++ front/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div>
+        <p>Project by : Anthony Bac / Jimmy Lai / Theovady Moutty</p>
+        <div></div>
+    </div>
+</footer>
---
+++ front/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="head">
+        <div>
+            <h1>SteelRoad</h1>
+        </div>
+        <div class="logb">
+            <a href="#" class="log">Sign-In</a>
+            <a href="#" class="log">Sign-Up</a>
+        </div>
+    </div>
+</header>
