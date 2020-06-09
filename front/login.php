6fd932f6b516f0acf100a1914880378e4d702fbe
Jimmy <ljimmy98@hotmail.fr>
2020-06-09 15:24:03 +0200

    login

--- front/login.php
+++ front/login.php
@@ -8,19 +8,27 @@
 <body>
 <?php
 include 'header.php';
+require '../back/autoform.php';
+require '../back/database.php';
 ?>
 <section>
     <article>
-        <form method="post" target="_blank">
+        <h1>Test Log</h1>
+        <form method="POST" target="_blank">
             <?php
-                $form = new autoform();
-
+            $form = new autoform();
+            //$req = new request('minesr_44703', 'TbhV1zzZ', 'ok', 'mysql', '178.32.113.35:3306');
+            $form->getInputText('Username', 'username');
+            $form->getInputPassword('Password', 'password');
+            $form->getInputSubmit('Connection');
             ?>
+            <p>pas encore inscrit ?</p>
+            <a href="/projetweb/front/register.php"><h3>S'inscrire</h3></a>
         </form>
     </article>
 </section>
 <?php
-include 'footer.php';
+    include 'footer.php';
 ?>
 </body>
 </html>
